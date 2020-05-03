d3856737ab161b108f1f6e855f1aa7ced33ef2be
Ricefab02 <roger.fabrice02@gmail.com>
2020-05-03 20:45:16 +0200

    build(gitignore): create with React

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import FormMovie from './FormMovie';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        < FormMovie/>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
